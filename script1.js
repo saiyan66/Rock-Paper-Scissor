596dbf6c01bbd2e0c3a22cf83cf78e6d9e61b9a1
sayan upadhyay <112376309+saiyan66@users.noreply.github.com>
2024-06-24 22:25:54 +0530

    Updated script by creating functions and assigning variables

--- script1.js
+++ script1.js
@@ -1,1 +1,41 @@
-console.log("Hello World!");
+function ComputerChoice(){
+    const choices = ["Rock", "Paper", "Scissor"];
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection) {
+        return("It's a Draw!");
+    }
+        else if ((playerSelection === "Rock") && (computerSelection === "Scissor")){
+            return("You Win! Rock beat Scissor");
+        }
+        
+        else if ((playerSelection === "Rock") && (computerSelection === "Paper")){
+            return("You Lose! Paper beat Rock")
+}
+
+        else if ((playerSelection === "Paper") && (computerSelection === "Rock")){
+            return("You Win! Paper beat Rock");
+        }
+
+        else if ((playerSelection === "Paper") && (computerSelection === "Scissor")){
+            return("You Lose! Scissor beat Paper");
+         }
+        
+        else if ((playerSelection === "Scissor") && (computerSelection === "Paper")){
+            return("You Win! Scissor beat Paper");
+
+        }  
+        else if ((playerSelection === "Scissor") && (computerSelection === "Rock")){
+            return("You Lose! Rock beat Scissor");
+        }
+}
+
+        let computerSelection= ComputerChoice();
+        let playerSelection= prompt("Choose your weapon:");
+        console.log(ComputerChoice());
+        console.log(playRound(playerSlection, computerSelection));
+
+
